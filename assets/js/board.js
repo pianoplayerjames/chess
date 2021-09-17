d14f8135cf42c8b7b8b2122e86e89982e13f56aa
pianoplayerjames <88819475+pianoplayerjames@users.noreply.github.com>
2021-09-17 17:19:54 +0100

    changes
    Resize now working. Working on the frontend UI and drag n drop.

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,9 +1,21 @@
 $(function() {
     draw_board();
+    $(".board").resizable({
+        handles: { se: '.fa-arrows-alt-h' },
+        aspectRatio: 1,
+        grid: 1,
+        minWidth: 200,
+        maxWidth: 650,
+        resize: function(e, ui) {
+            var grid = ui.element.width().toFixed(0) / 8;
+            $( this ).css( "grid-template-columns", "repeat(8, "+grid+"px)" );
+            $( this ).css( "grid-template-rows", "repeat(8, "+grid+"px)" );
+        }
+    });
     $('.piece').draggable({
         distance: 0,
         revert: "invalid",
-        revertDuration: 100,
+        revertDuration: 0,
         containment: "window",
         start: function(event, ui){
             $(this).draggable('instance').offset.click = {
@@ -16,10 +28,11 @@ $(function() {
         accept: ".piece",
         revert: "invalid",
         drop: function(event, ui) {
-            var draggableId = ui.draggable.attr("id");
+            var draggableId = ui.draggable.attr("data-square");
             var droppableId = $(this).attr("id");
 
-            console.log(draggableId + " to " + droppableId);
+            $('#'+droppableId).append(ui.draggable);
+            $('#'+ui.draggable).css({'top' : '0', 'left' : '0'});
         }
       });
 });
@@ -34,38 +47,38 @@ function draw_board() {
             const number = j + i + 2;
     
             if(number % 2 === 0) {
-                $('.board').append('<div id="'+file[i]+rank[j]+'" data-square="'+file[i]+rank[j]+'" data-file="' + file[i] + '" data-rank="' + rank[j] + '" class="tile p-1 black-tile"></div>');
+                $('.board').append('<div id="'+file[i]+rank[j]+'" data-square="'+file[i]+rank[j]+'" class="tile black-tile"></div>');
             } else {
-                $('.board').append('<div id="'+file[i]+rank[j]+'" data-square="'+file[i]+rank[j]+'" data-file="' + file[i] + '" data-rank="' + rank[j] + '" class="tile p-1 white-tile"></div>');
+                $('.board').append('<div id="'+file[i]+rank[j]+'" data-square="'+file[i]+rank[j]+'" class="tile white-tile"></div>');
             }
         }
     }
 
     // white pieces
     for(let p = 0; p <= 8; p++) {
-        $('.tile[data-square="' + file[p] + '2"]').html("<img src='assets/img/pieces/w_pawn.svg' class='piece' width='100%' />");
+        $('.tile[data-square="' + file[p] + '2"]').html("<img data-square='"+ file[p] +"2' src='assets/img/pieces/w_pawn.svg' class='piece' width='100%' />");
     }
-    $('.tile[data-square="a1').html("<img src='assets/img/pieces/w_rook.svg' class='piece' width='100%' />");
-    $('.tile[data-square="b1').html("<img src='assets/img/pieces/w_knight.svg' class='piece' width='100%' />");
-    $('.tile[data-square="c1').html("<img src='assets/img/pieces/w_bishop.svg' class='piece' width='100%' />");
-    $('.tile[data-square="d1').html("<img src='assets/img/pieces/w_queen.svg' class='piece' width='100%' />");
-    $('.tile[data-square="e1').html("<img src='assets/img/pieces/w_king.svg' class='piece' width='100%' />");
-    $('.tile[data-square="f1').html("<img src='assets/img/pieces/w_bishop.svg' class='piece' width='100%' />");
-    $('.tile[data-square="g1').html("<img src='assets/img/pieces/w_knight.svg' class='piece' width='100%' />");
-    $('.tile[data-square="h1').html("<img src='assets/img/pieces/w_rook.svg' class='piece' width='100%' />");
+    $('.tile[data-square="a1').html("<img data-square='a1' src='assets/img/pieces/w_rook.svg' class='piece' width='100%' />");
+    $('.tile[data-square="b1').html("<img data-square='b1' src='assets/img/pieces/w_knight.svg' class='piece' width='100%' />");
+    $('.tile[data-square="c1').html("<img data-square='c1' src='assets/img/pieces/w_bishop.svg' class='piece' width='100%' />");
+    $('.tile[data-square="d1').html("<img data-square='d1' src='assets/img/pieces/w_queen.svg' class='piece' width='100%' />");
+    $('.tile[data-square="e1').html("<img data-square='e1' src='assets/img/pieces/w_king.svg' class='piece' width='100%' />");
+    $('.tile[data-square="f1').html("<img data-square='f1' src='assets/img/pieces/w_bishop.svg' class='piece' width='100%' />");
+    $('.tile[data-square="g1').html("<img data-square='g1' src='assets/img/pieces/w_knight.svg' class='piece' width='100%' />");
+    $('.tile[data-square="h1').html("<img data-square='h1' src='assets/img/pieces/w_rook.svg' class='piece' width='100%' />");
 
     // black pieces
     for(let p = 0; p <= 8; p++) {
-        $('.tile[data-square="' + file[p] + '7"]').html("<img src='assets/img/pieces/b_pawn.svg' class='piece' width='100%' />");
+        $('.tile[data-square="' + file[p] + '7"]').html("<img data-square='"+ file[p] +"7' src='assets/img/pieces/b_pawn.svg' class='piece' width='100%' />");
     }
-    $('.tile[data-square="a8').html("<img src='assets/img/pieces/b_rook.svg' class='piece' width='100%' />");
-    $('.tile[data-square="b8').html("<img src='assets/img/pieces/b_knight.svg' class='piece' width='100%' />");
-    $('.tile[data-square="c8').html("<img src='assets/img/pieces/b_bishop.svg' class='piece' width='100%' />");
-    $('.tile[data-square="d8').html("<img src='assets/img/pieces/b_queen.svg' class='piece' width='100%' />");
-    $('.tile[data-square="e8').html("<img src='assets/img/pieces/b_king.svg' class='piece' width='100%' />");
-    $('.tile[data-square="f8').html("<img src='assets/img/pieces/b_bishop.svg' class='piece' width='100%' />");
-    $('.tile[data-square="g8').html("<img src='assets/img/pieces/b_knight.svg' class='piece' width='100%' />");
-    $('.tile[data-square="h8').html("<img src='assets/img/pieces/b_rook.svg' class='piece' width='100%' />");
+    $('.tile[data-square="a8').html("<img data-square='a8' src='assets/img/pieces/b_rook.svg' class='piece' width='100%' />");
+    $('.tile[data-square="b8').html("<img data-square='b8' src='assets/img/pieces/b_knight.svg' class='piece' width='100%' />");
+    $('.tile[data-square="c8').html("<img data-square='c8' src='assets/img/pieces/b_bishop.svg' class='piece' width='100%' />");
+    $('.tile[data-square="d8').html("<img data-square='d8' src='assets/img/pieces/b_queen.svg' class='piece' width='100%' />");
+    $('.tile[data-square="e8').html("<img data-square='e8' src='assets/img/pieces/b_king.svg' class='piece' width='100%' />");
+    $('.tile[data-square="f8').html("<img data-square='f8' src='assets/img/pieces/b_bishop.svg' class='piece' width='100%' />");
+    $('.tile[data-square="g8').html("<img data-square='g8' src='assets/img/pieces/b_knight.svg' class='piece' width='100%' />");
+    $('.tile[data-square="h8').html("<img data-square='h8' src='assets/img/pieces/b_rook.svg' class='piece' width='100%' />");
 
 }
 
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Chess</title>
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/fontawesome/css/all.min.css" />
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/jquery-ui.min.js"></script>
@@ -16,11 +17,54 @@
 </head>
 
 <body>
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-dark" style='background: #0000005c;'>
+  <div class="container">
+    <a class="navbar-brand" href="#">Chess</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Lobby</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Training</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Puzzles
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Puzzles</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Community</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav mb-2 mb-lg-0">
+      <li class="nav-item">
+          <a class="nav-link" href="#">Settings</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">James</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+    <div class="container">
         <div class="row">
-            <div class="col-lg-6">
-                <div class="board" data-axis="white" data-game='fb48bgb'></div>
-                <div class='btn btn-success' onclick='flip();'>flip board</div>
+            <div class="col-lg-12 mt-3">
+                <div class="board position-relative" data-axis="white" data-game='fb48bgb'>
+                    <div class="position-absolute top-100 start-100 p-1 translate-middle fas fa-arrows-alt-h ms-3 shadow" style='margin-top: -12px;'></div>
+                </div>
             </div>
         </div>
     </div>
